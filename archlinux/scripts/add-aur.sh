9aadc05ecca6630e7a64b25c82c2c715554e3520
flagarde <flagarde.programs@pm.me>
2022-04-27 21:24:21 +0800

    Fix

--- archlinux/scripts/add-aur.sh
+++ archlinux/scripts/add-aur.sh
@@ -58,52 +58,11 @@ FPP=$(dirname "${FOREIGN_PKG}")
 mkdir -p "${FPP}"
 install -o "${AUR_USER}" -d "${FOREIGN_PKG}"
 
-echo '# Maintainer: Colin Woodbury <colin@fosskers.ca>\n
-pkgname=aura-bin\n
-pkgver=3.2.8\n
-pkgrel=1\n
-pkgdesc=\"A secure package manager for Arch Linux and the AUR - Prebuilt binary\"
-url=\"https://github.com/fosskers/aura\"
-license=('GPL3')
-arch=('x86_64')
-depends=('gmp' 'pacman')
-optdepends=('bash-completion: for bash completions')
-provides=('aura')
-conflicts=('aura' 'aura-git')
-options=('strip')
-backup=(\"etc/aura.conf\")
-source=(\"aura-$pkgver-x86_64.tar.gz::$url/releases/download/v$pkgver/aura-$pkgver-x86_64.tar.gz\")
-sha256sums=('f77eb9d20338c3f15b8df9fc02fe47222b370fe20169e229855452ffd032f68b')
-
-package() {
-    # Install aura binary
-    install -Dm755 aura -t \"$pkgdir/usr/bin/\"
-
-    # Installing conf file
-    install -Dm644 aura.conf -t \"$pkgdir/etc\"
-
-    # Installing man pages
-    install -Dm644 aura.8 -t \"$pkgdir/usr/share/man/man8\"
-    install -Dm644 aura.conf.5 -t \"$pkgdir/usr/share/man/man5\"
-
-    # Installing bash completions
-    install -Dm644 bashcompletion.sh \
-      \"$pkgdir/usr/share/bash-completion/completions/aura\"
-
-    # Installing zsh completions
-    install -Dm644 _aura -t \
-      \"$pkgdir/usr/share/zsh/site-functions\"
-
-    # Directories for storing PKGBUILDs, source packages & installed package states
-    install -d \"$pkgdir/var/cache/aura/\"{pkgbuilds,src,states}
-}' >> PKGBUILD
-
 # get helper pkgbuild
-#sudo -u "${AUR_USER}" -D~ bash -c "curl --silent --location https://aur.archlinux.org/cgit/aur.git/snapshot/aura-bin.tar.gz | bsdtar -xvf -"
+sudo -u "${AUR_USER}" -D~ bash -c "curl --silent --location https://raw.githubusercontent.com/flagarde/ci_bak/archlinux/scripts/PKGBUILD"
 
 # make helper
-sudo -u "${AUR_USER}" -D~ bash -c "makepkg -s --noprogressbar --noconfirm --needed"
-
+sudo -u "${AUR_USER}" -D~//${HELPER} bash -c "makepkg -s --noprogressbar --noconfirm --needed"
 
 # install helper
 pacman --upgrade --needed --noconfirm --noprogressbar "${NEW_PKGDEST}"/*.pkg.*
