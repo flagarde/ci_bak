884f0e431db79aa80d96f59f92adc9de45c4c06f
flagarde <flagarde.programs@pm.me>
2022-04-27 17:58:16 +0800

    Test

--- ubuntu/scripts/bootstrap.sh
+++ ubuntu/scripts/bootstrap.sh
@@ -26,25 +26,36 @@ echo "deb http://archive.ubuntu.com/ubuntu/ kinetic main" | tee -a /etc/apt/sour
 echo "deb http://archive.ubuntu.com/ubuntu/ kinetic universe" | tee -a /etc/apt/sources.list.d/Ubuntu.list
 echo "deb http://archive.ubuntu.com/ubuntu/ kinetic-updates main" | tee /etc/apt/sources.list.d/Ubuntu.list
 echo "deb http://archive.ubuntu.com/ubuntu/ kinetic-updates universe" | tee -a /etc/apt/sources.list.d/Ubuntu.list
+##
+echo "deb https://ppa.launchpadcontent.net/ubuntu-toolchain-r/ppa/ubuntu jammy main"  | tee /etc/apt/sources.list.d/Toolchain.list
+echo "deb-src https://ppa.launchpadcontent.net/ubuntu-toolchain-r/ppa/ubuntu jammy main"  | tee /etc/apt/sources.list.d/Toolchain.list
+## Intel oneAPI
+echo "deb [signed-by=/usr/share/keyrings/oneAPI.gpg] https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
+## LLVM
+echo "deb [signed-by=/usr/share/keyrings/LLVM.gpg] http://apt.llvm.org/jammy/ llvm-toolchain-jammy main" | tee /etc/apt/sources.list.d/LLVM.list
+echo "deb [signed-by=/usr/share/keyrings/LLVM.gpg] http://apt.llvm.org/jammy/ llvm-toolchain-jammy-13 main" | tee -a /etc/apt/sources.list.d/LLVM.list
+echo "deb [signed-by=/usr/share/keyrings/LLVM.gpg] http://apt.llvm.org/jammy/ llvm-toolchain-jammy-14 main" | tee -a /etc/apt/sources.list.d/LLVM.list
+## CMake
+echo 'deb [signed-by=/usr/share/keyrings/CMake.gpg] https://apt.kitware.com/ubuntu/ jammy main' | tee /etc/apt/sources.list.d/kitware.list
+echo 'deb [signed-by=/usr/share/keyrings/CMake.gpg] https://apt.kitware.com/ubuntu/ jammy-rc main' | tee -a /etc/apt/sources.list.d/kitware.list
 
 # First download ca-certificates
 apt-get --allow-unauthenticated update
+apt-get --allow-unauthenticated dist-upgrade
 apt-get install -y --no-install-recommends --allow-unauthenticated gpg ca-certificates wget apt-utils
 
 ## Intel oneAPI
-wget -O - https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor - | tee /usr/share/keyrings/oneAPI.gpg
-echo "deb [signed-by=/usr/share/keyrings/oneAPI.gpg] https://apt.repos.intel.com/oneapi all main" | tee -a /etc/apt/sources.list.d/oneAPI.list
+#wget -O - https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor - | tee /usr/share/keyrings/oneAPI.gpg
+
 ## LLVM
 #wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | gpg --dearmor - | tee /usr/share/keyrings/LLVM.gpg
-#echo "deb [signed-by=/usr/share/keyrings/LLVM.gpg] http://apt.llvm.org/focal/ llvm-toolchain-focal main" | tee /etc/apt/sources.list.d/LLVM.list
-#echo "deb [signed-by=/usr/share/keyrings/LLVM.gpg] http://apt.llvm.org/focal/ llvm-toolchain-focal-13 main" | tee -a /etc/apt/sources.list.d/LLVM.list
-#echo "deb [signed-by=/usr/share/keyrings/LLVM.gpg] http://apt.llvm.org/focal/ llvm-toolchain-focal-14 main" | tee -a /etc/apt/sources.list.d/LLVM.list
+
 ## CMake
 #wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc | gpg --dearmor - | tee /usr/share/keyrings/CMake.gpg >/dev/null
 #echo 'deb [signed-by=/usr/share/keyrings/CMake.gpg] https://apt.kitware.com/ubuntu/ focal main' | tee -a /etc/apt/sources.list.d/kitware.list
 #echo 'deb [signed-by=/usr/share/keyrings/CMake.gpg] https://apt.kitware.com/ubuntu/ focal-rc main' | tee -a /etc/apt/sources.list.d/kitware.list
 
-apt-get update
+#apt-get update
 #apt-get dist-upgrade -y
 #apt-get install --no-install-recommends -y software-properties-common wget ca-certificates apt-utils gpg-agent make binutils
 #add-apt-repository -y ppa:ubuntu-toolchain-r/test
