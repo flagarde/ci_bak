8269daf65a567e9a63aae05eccb512f927257f9c
flagarde <flagarde.programs@pm.me>
2022-04-27 16:34:19 +0800

    Test

--- ubuntu/scripts/bootstrap.sh
+++ ubuntu/scripts/bootstrap.sh
@@ -2,7 +2,7 @@
 
 # First download ca-certificates
 apt-get --allow-unauthenticated update
-apt-get install -y --no-install-recommends --allow-unauthenticated ca-certificates dirmngr
+apt-get install -y --no-install-recommends --allow-unauthenticated ca-certificates gpg dirmngr
 
 # Add keys
 
