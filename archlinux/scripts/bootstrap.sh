8503212ebeb4a25d2465dd548333d216cb0a9861
flagarde <flagarde.programs@pm.me>
2022-04-27 19:37:45 +0800

    Test

--- archlinux/scripts/bootstrap.sh
+++ archlinux/scripts/bootstrap.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-useradd --no-create-home --shell=/bin/false build && usermod -L build
-echo "build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 makepkg ./archlinux/aurora-bin
